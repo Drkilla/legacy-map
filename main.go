2df5a9357aa24f53c566b52d094bd1a5e6f71fb3
sandarwsl <drkilla@DESKTOP-CULGR52.>
2026-03-30 22:11:00 +0200

    fix: read version from Go module info for go install

--- main.go
+++ main.go
@@ -1,10 +1,19 @@
 package main
 
-import "github.com/drkilla/legacy-map/cmd"
+import (
+	"runtime/debug"
+
+	"github.com/drkilla/legacy-map/cmd"
+)
 
 var version = "dev"
 
 func main() {
+	if version == "dev" {
+		if info, ok := debug.ReadBuildInfo(); ok && info.Main.Version != "" && info.Main.Version != "(devel)" {
+			version = info.Main.Version
+		}
+	}
 	cmd.SetVersion(version)
 	cmd.Execute()
 }
